9804bdcd4390088dfa96575d5c337ca96f1651c3
Fhellipe Valentin <fhellipereis@outlook.com>
2022-10-04 11:48:00 -0300

    Adicionando Tarefas

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Tasks } from 'src/app/model/tasks';
 import { TaskService } from 'src/app/services/task.service';
 
@@ -12,16 +12,33 @@ export class TaskListComponent implements OnInit {
 
   constructor( private taskService: TaskService, private formBilder: FormBuilder) { }
   
-  tasksList: any = [] 
+  formulary!: FormGroup;
+
+  tasksList: any = []
   columns : string[] = ['author', 'title', 'created_At', 'status', 'desc']
 
   ngOnInit(): void {
-    this.showData()
+    this.showData();
+    this.formulary = this.formBilder.group( {
+      author: new FormControl('', Validators.required),
+      title: new FormControl('', Validators.required),
+      desc: new FormControl('', Validators.required)
+    })
+
   }
 
   showData() {
     this.taskService.listDataTasks().subscribe((data: {})=> {
       this.tasksList = data
+    })
+  }
+
+  submit() {
+    const formValues = this.formulary.value
+    const tasks: Tasks = new Tasks(formValues.author, formValues.title, formValues.desc, new Date().toUTCString(), false);
+    this.taskService.insertTask(tasks).subscribe(response => {
+      this.tasksList.push(response)
+      console.log(response)
     })
   }
 }
--- src/app/model/tasks.ts
+++ src/app/model/tasks.ts
@@ -1,9 +1,17 @@
-export interface Tasks {
-    id: number;
+export class Tasks {
+    id!: number;
     author: string;
     title: string;
-    created_At: Date;
-    expiration: Date;
-    status: false;
+    created_At: string;
+    expiration!: Date;
+    status!: false;
     desc: string;
+
+    constructor(author: string, title: string, desc: string, created_At: string, status: false) {
+        this.author = author;
+        this.title = title;
+        this.desc = desc;
+        this.created_At = created_At;
+        this.status = false;
+    }
 }
